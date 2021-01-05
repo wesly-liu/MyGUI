5dad7954048d2476058970ad178a8c9554444b99
Liu Wei <welsy_liu@sina.com>
2021-01-05 16:23:57 +0800

    Just had done some minor remarks to practice git

--- mytable.py
+++ mytable.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import wx
 import wx.grid
+from operator import itemgetter#用于二维列表排序
 
 class MyTable(wx.grid.Grid):
     def __init__(self,parent):
@@ -31,19 +32,20 @@ class MyTable(wx.grid.Grid):
         return result
     
     def setRowCount(self,row):
+        '''设置表的行数，列保持不变'''
         c=self.GetNumberCols()
         self.SetTable(None)
         self.CreateGrid(row,c)
         
     def setColumnCount(self,col):
+        '''设置表的列数，行保持不变'''
         r=self.GetNumberRows()
         self.SetTable(None)
         self.CreateGrid(r,col)
         
     def clearRows(self):
-        c=self.GetNumberCols()
-        self.SetTable(None)
-        self.CreateGrid(0,c)
+        '''清除行，列不变'''
+        self.DeleteRows(0,self.GetNumberRows())
 
     def appendList(self,target):
         '''把一个二维List追加到表格,保留原来的数据'''       
@@ -56,6 +58,25 @@ class MyTable(wx.grid.Grid):
         for i in range(0,row):
             for j in range(0,col):
                 self.SetCellValue(currentRow+i,j,target[i][j])
+                
+    def SetOrderBy(self,column=0,desc=False,number=False):
+        '''按列排序'''
+        currentCol=column
+        result=[]
+        row=self.GetNumberRows()
+        col=self.GetNumberCols()
+        for i in range(0,row):
+            rowValue=[]
+            for j in range(0,col):
+                rowValue.append(self.GetCellValue(i,j))
+            result.append(rowValue)
+        if number==True:
+            for r in result:
+                r[currentCol]=int(r[currentCol])
+        result.sort(key=itemgetter(currentCol),reverse=desc)
+        for i in range(0,row):
+            for j in range(0,col):
+                self.SetCellValue(i,j,str(result[i][j]))
 
 #below are sample class and main        
 class MyFrame(wx.Frame):
@@ -66,13 +87,15 @@ class MyFrame(wx.Frame):
 
         grid=MyTable( p )
         
-        ls=[['1','2','3'],['4','5','6']]
+        ls=[['11','2','3'],['41','5','6']]
         grid.setList(ls)
 
         ls1=[['12','22','32'],['42','52','62']]
-        returnList=grid.appendList(ls1)
-        print(returnList)
-
+        grid.appendList(ls1)
+        ls2=grid.getList()
+        print(ls2)
+        
+        grid.SetColumnsOrder(0)
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(grid, 1, wx.EXPAND|wx.ALL, 5)
         p.SetSizer(sizer)
